f93c3898fd5e229e3bc3e2a55ad888012c13283e
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-08-18 18:24:09 +0500

    add

--- bot/bot.js
+++ bot/bot.js
@@ -1,19 +1,59 @@
-import TelegramBot from "node-telegram-bot-api";
-import "dotenv/config";
+// import TelegramBot from "node-telegram-bot-api";
+// import "dotenv/config";
+// import { namozOption, regionOptions } from "../options.js";
+// import { regionTime } from "../regions/region.js";
+
+
+
+
+// export const startBot = (bot) => {
+//   bot.setMyCommands([
+//     { command: "/start", description: "Boshlash" },
+//   ]);
+//   bot.on("message", async (msg) => {
+//     const text = msg.text;
+//     const chatId = msg.chat.id;
+    
+
+//     const opts = {
+//       reply_to_message_id: msg.message_id,
+//       reply_markup: {
+//         resize_keyboard: true,
+//         one_time_keyboard: true,
+//         keyboard: [['Namoz vaqtlari']]
+//       }
+//     };
+//     if (text === "/start") {
+//       await bot.sendMessage(chatId, "Botimizga hush kelibsiz!", opts)
+//     }
+//     if (text === 'Namoz vaqtlari') {
+//       await bot.sendMessage(chatId, "Hududni tanlang", regionOptions)
+//     }
+//     else{
+//       await bot.sendMessage(chatId, "Tugmadan foydalaning")
+//     }
+//   });
+//   bot.on('callback_query', async msg => {
+//     const data = msg.data
+//     const chatId = msg.message.chat.id
+//     await regionTime(data, chatId, bot)
+//   })
+
+
+// };
+
+  
 import { namozOption, regionOptions } from "../options.js";
 import { regionTime } from "../regions/region.js";
-
-
-
 
 export const startBot = (bot) => {
   bot.setMyCommands([
     { command: "/start", description: "Boshlash" },
   ]);
+
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-    
 
     const opts = {
       reply_to_message_id: msg.message_id,
@@ -23,23 +63,19 @@ export const startBot = (bot) => {
         keyboard: [['Namoz vaqtlari']]
       }
     };
+
     if (text === "/start") {
-      await bot.sendMessage(chatId, "Botimizga hush kelibsiz!", opts)
-    }
-    if (text === 'Namoz vaqtlari') {
-      await bot.sendMessage(chatId, "Hududni tanlang", regionOptions)
-    }
-    else{
-      await bot.sendMessage(chatId, "Tugmadan foydalaning")
+      await bot.sendMessage(chatId, "Botimizga hush kelibsiz!", opts);
+    } else if (text === 'Namoz vaqtlari') {
+      await bot.sendMessage(chatId, "Hududni tanlang", regionOptions);
+    } else {
+      await bot.sendMessage(chatId, "Tugmadan foydalaning");
     }
   });
+
   bot.on('callback_query', async msg => {
-    const data = msg.data
-    const chatId = msg.message.chat.id
-    await regionTime(data, chatId, bot)
-  })
-
-
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    await regionTime(data, chatId, bot);
+  });
 };
-
-  
--- index.js
+++ index.js
@@ -1,17 +1,56 @@
+// import express from 'express';
+// import { startBot } from './bot/bot.js';
+// import 'dotenv/config';
+// import bodyParser from "body-parser";
+// import TelegramBot from 'node-telegram-bot-api';
+
+// const app = express();
+// const TOKEN = process.env.TELEGRAM_TOKEN;
+// const URL = 'https://namozvaqtlari.onrender.com'
+// const PORT = process.env.PORT || 3000;
+
+// startBot(bot);
+
+// const bot = new TelegramBot(TOKEN, { polling: false });
+
+// app.get("/ping", (req, res) => {
+//   res.send("Bot ishladi !");
+// });
+
+// app.use(bodyParser.json());
+
+// app.post("/" + TOKEN, (req, res) => {
+//   bot.processUpdate(req.body);
+//   res.sendStatus(200);
+// });
+
+// bot.onText(/\/start/, (msg) => {
+//   bot.sendMessage(msg.chat.id, "Salom, Nodirbek! 🚀 Men ishlayapman!");
+// });
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+//   bot.setWebHook(`${URL}/${TOKEN}`);
+// });
+ 
+
+
+
+
 import express from 'express';
-import { startBot } from './bot/bot.js';
 import 'dotenv/config';
 import bodyParser from "body-parser";
 import TelegramBot from 'node-telegram-bot-api';
+import { startBot } from './bot/bot.js';
 
 const app = express();
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const URL = 'https://namozvaqtlari.onrender.com'
+const URL = 'https://namozvaqtlari.onrender.com';
 const PORT = process.env.PORT || 3000;
 
-startBot(bot);
+const bot = new TelegramBot(TOKEN, { polling: false }); // faqat webhook
 
-const bot = new TelegramBot(TOKEN, { polling: false });
+startBot(bot);
 
 app.get("/ping", (req, res) => {
   res.send("Bot ishladi !");
@@ -24,12 +63,8 @@ app.post("/" + TOKEN, (req, res) => {
   res.sendStatus(200);
 });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Salom, Nodirbek! 🚀 Men ishlayapman!");
-});
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Server running on port", PORT);
-  bot.setWebHook(`${URL}/${TOKEN}`);
+  await bot.deleteWebHook({ drop_pending_updates: true }); // eski webhooklarni o‘chir
+  await bot.setWebHook(`${URL}/${TOKEN}`);
 });
- 
